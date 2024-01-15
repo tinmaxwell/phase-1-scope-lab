de040aa8559020aa68bbbc82940f565aff5890f8
tinmaxwell <tinmaxwell@gmail.com>
2024-01-15 13:46:59 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+ customerName = customerName.toUpperCase();
+}
+
+function SetBestCustomer() {
+ var bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer() {
+ bestCustomer = 'maybe bob';
+} 
+
+const leastFavoriteCustomer = 'Jim';
+
+function changeLeastFavoriteCustomer() {
+ leastFavoriteCustomer = 'Tom'   
+}
